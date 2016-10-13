4dc809b8beb71774c2791fc51b90364eff54bb0d
協調メディア研究室 <cmlab@kyouchoumediakenkyuushitsu-no-macbook-pro.local>
2016-10-13 15:15:02 +0900

    CPの追加

--- inoshika/inoshika/GameViewController.swift
+++ inoshika/inoshika/GameViewController.swift
@@ -13,36 +13,54 @@ import SceneKit
 
 
 class Player {
-    var player_name : String = ""                                //player番号
-    var tehuda : [Int]=[11,103,84,22,74,83,54,44]     //持ち札
-    var syutoku : [Int]=[14,34,84,114,123,11,21,31,41,51,61,71,81,91,101,64,74,104,0,0,0,0]          //取得札
-    var tehuda_nokori = 8                            //持ち札の残り枚数
-    var syutoku_maisu = 18                          //取得札の枚数
+    var player_num : Int = 0                                //player番号
+    var tehuda : [Int]=[11,103,84,22,74,83,54,44]           //持ち札
+    var syutoku : [Int]=[1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]          //取得札
+    var tehuda_max = 8                              //持ち札の残り枚数
+    var tehuda_cnt = 0                              //手札の残数のカウント用
+    var syutoku_maisu = 2                           //取得札の枚数
     var points:Int = 0                              //得点
-    var yaku_check:Int = 0                          //役判定
+    var yaku_cnt:Int = 0                            //役判定
     var select_tnum: Int = 0                        //手札の選択
     var select_bnum: Int = 0                        //場札の選択
-    var flag = 0
+    var yc_flag = 0                                 //役判定用flag
+    var cp_flag = 0                                 //cp用flag
 
     
     
-    func Check_points() -> Void {                      //得点のチェック
-        print(player_name, "の得点は",points,"点です。")
+    func Check_points() -> Void {                         //得点のチェック
+        print("player",player_num, "の得点は",points,"点です。")
     }
     
     
     func Tehuda_print() -> Void {                         //持ち札のチェック
-        print(player_name, "の")
+        print("player", player_num, "の")
         
-        for i in 0..<tehuda_nokori{
+        for i in 0..<tehuda_max{
             
             print(i+1,"枚目の持ち札は",huda[tehuda[i]])
         
         }
     }
+    
+    func Check_tehuda() -> Int {                          //手札の枚数をカウントする関数
+        
+        tehuda_cnt = 0
+        
+        for i in 0 ..< 8 {
+            
+            if tehuda[i] == 0 {
+                tehuda_cnt += 1
+            }
+        }
+        
+        return tehuda_cnt
+    }
+    
  
     
-    func Check_yaku() -> Void {                           //役のチェック
+    func Check_yaku() -> Void {                           //役のチェック関数
+        yc_flag = 0
         
         Goko_check()
         Kasu_check()
@@ -51,31 +69,31 @@ class Player {
         
     }
     
-    func Goko_check() -> Void {                           //五光のチェック
+    func Goko_check() -> Void {                           //五光のチェック関数
 
         
         for i in 0..<syutoku_maisu{                       //五光札の枚数をカウントしyaku_checkに格納
             
             if huda[syutoku[i]] == "五光"{
-                yaku_check = yaku_check+1
+                yaku_cnt = yaku_cnt+1
             }
         }
         
-        switch yaku_check{                                //yaku_checkにより役を識別、得点を加算
+        switch yaku_cnt{                                  //yaku_checkにより役を識別、得点を加算
         case 3:
             print("三光です")
             points = points + 6
         case 4:
-            flag = 0
+            yc_flag = 0
             
             for i in 0..<syutoku_maisu{
                 if syutoku[i] == 114 {
-                    flag = 1
+                    yc_flag = 1
                     break
                 }
             }
             
-            if flag == 1 {
+            if yc_flag == 1 {
                 print("雨四光です")
                 points = points + 8
             } else {
@@ -86,80 +104,93 @@ class Player {
             print("五光です")
             points = points + 15
         default:
-            print(yaku_check)
+            print(yaku_cnt)
         }
         
         
     }
     
-    func Kasu_check() -> Void {                            //カスのチェック
-        yaku_check = 0
+    func Kasu_check() -> Void {                            //カスのチェック関数
+        yaku_cnt = 0
         
         for i in 0..<syutoku_maisu{                        //カス札の枚数をカウントしyaku_checkに格納
             
             if huda[syutoku[i]] == "カス"{
-                yaku_check = yaku_check + 1
+                yaku_cnt = yaku_cnt + 1
             }
         }
         
-       if yaku_check > 9{                                  //カス札の枚数に応じた得点を加算
-            points = points + (yaku_check-9)
+       if yaku_cnt > 9{                                    //カス札の枚数に応じた得点を加算
+            points = points + (yaku_cnt-9)
             print("カスです")
         }
     }
     
-    func Tan_check() -> Void {                             //タンのチェック
-        yaku_check = 0
+    func Tan_check() -> Void {                             //タンのチェック関数
+        yaku_cnt = 0
         
         for i in 0..<syutoku_maisu{                        //短冊札の枚数をカウントしyaku_checkに格納
             
             if huda[syutoku[i]] == "短冊"{
-                yaku_check = yaku_check + 1
+                yaku_cnt = yaku_cnt + 1
             }
         }
         
-        if yaku_check > 4{                                 //短冊札の枚数に応じた得点を加算
-            points = points + (yaku_check-4)
+        if yaku_cnt > 4{                                   //短冊札の枚数に応じた得点を加算
+            points = points + (yaku_cnt-4)
             print("短冊です")
         }
     }
     
-    func Tane_check() -> Void {                             //タネのチェック
-        yaku_check = 0
+    func Tane_check() -> Void {                             //タネのチェック関数
+        yaku_cnt = 0
         
         for i in 0..<syutoku_maisu{                         //タネ札の枚数をカウントしyaku_checkに格納
             
             if huda[syutoku[i]] == "タネ"{
-                yaku_check = yaku_check + 1
+                yaku_cnt = yaku_cnt + 1
             }
         }
-        if yaku_check > 4{                                  //タネ札の枚数に応じた得点を加算
-            points = points + (yaku_check-4)
+        if yaku_cnt > 4{                                    //タネ札の枚数に応じた得点を加算
+            points = points + (yaku_cnt - 4)
             print("タネです")
         }
     }
     
-    func InoShikaCyo() -> Void {                            //猪鹿蝶のチェック
-        yaku_check = 0
+    func InoShikaCyo() -> Void {                            //猪鹿蝶のチェック関数
+        yaku_cnt = 0
         
         for i in 0..<syutoku_maisu{                         //猪・鹿・蝶の札の枚数をカウントしyaku_checkに格納
             
             if syutoku[i] == 64{
-                yaku_check = yaku_check + 1
+                yaku_cnt = yaku_cnt + 1
             }else if syutoku[i] == 74{
-                yaku_check = yaku_check + 1
+                yaku_cnt = yaku_cnt + 1
             }else if syutoku[i] == 104{
-                yaku_check = yaku_check + 1
+                yaku_cnt = yaku_cnt + 1
             }
         }
         
-        if yaku_check == 3{                                 //猪鹿蝶の得点を加算
+        if yaku_cnt == 3{                                    //猪鹿蝶の得点を加算
             points = points + 5
             print("猪鹿蝶です")
         }
     }
     
-    func Add_syutoku_tb(select_tnum: Int, select_bnum: Int) -> Int {   //選択した手札と場札の番号を受け取りそれぞれを取得配列に格納
+    
+    
+    func Add_syutoku_tb(select_tnum: Int, select_bnum: Int) -> Int {   //選択した手札と場札の番号を受け取りそれぞれを取得配列に格納する関数
+        
+        if tehuda[select_tnum] / 10 != table.bahuda[select_bnum] / 10 { //手札と場札が合わない場合1を返り値とする
+            
+            return 1
+        }
+        
+        if tehuda[select_tnum] == 0 {                                   //選択した手札が空行列の時1を返り値とする
+            
+            return 1
+        }
+        
         
         syutoku[syutoku_maisu] = tehuda[select_tnum]
         tehuda[select_tnum] = 0
@@ -176,12 +207,70 @@ class Player {
         
     }
     
-    func Add_syutoku_yb(select_y: Int, select_b: Int) -> Void {
+    func Add_syutoku_yb(select_y: Int, select_b: Int) -> Void {         //手札と山札の組を取得配列に格納する関数
         
         syutoku[syutoku_maisu] = select_y
         syutoku_maisu += 1
         syutoku[syutoku_maisu] = select_b
         syutoku_maisu += 1
+        
+    }
+    
+    func Add_bahuda_t(select_t: Int) -> Int {                           //手札を場札配列の空きに挿入する関数
+        
+        for i in 0 ..< 8 {
+            
+            if table.bahuda[i] == 0 {
+                table.bahuda[i] = tehuda[select_t]
+                return 0
+            }
+        }
+        
+        return 1
+        
+    }
+    
+
+    
+    
+    func CP() -> Void {                                                 //CPの動作決定関数
+        
+        for i in 0 ..< 8 {
+        
+            for j in 0 ..< 8 {
+            
+                 cp_flag = Add_syutoku_tb(i, select_bnum: j)
+                
+                if cp_flag == 0 {
+                    print("test")
+                    break
+                }
+                
+            }
+            
+            if cp_flag == 0 {
+                print("test2")
+
+                break
+            }
+            
+            
+        }
+        
+        
+        if cp_flag == 1 {
+            
+            for i in 0 ..< 8 {
+                
+                if tehuda[i] != 0 {
+                    Add_bahuda_t(tehuda[i])
+                    break
+                }
+            }
+        }
+        
+        table.Draw(1)
+
         
     }
     
@@ -193,26 +282,26 @@ class Player {
 
 
 
+
 class Table {
     var base : [Int] = [11, 12, 13, 14, 21, 22, 23, 24, 31, 32, 33, 34, 41, 42, 43, 44,
                         51, 52, 53, 54, 61, 62, 63, 64, 71, 72, 73, 74, 81, 82, 83, 84,
-                        91, 92, 93, 94, 101, 102, 103, 104, 111, 112, 113, 114, 121, 122, 123, 124]
-    var shuffle_base : [Int] = [ ]
-    var yamahuda : [Int] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,51,52] //山札
-    var bahuda : [Int] = [73,14,12,13,44,34,0,0]                           //場札
-    var bahuda_nokori = 6                                                    //場札残り
-    var yamahuda_nokori = 24                                                 //山札残り
+                        91, 92, 93, 94, 101, 102, 103, 104, 111, 112, 113, 114, 121, 122, 123, 124] //base配列
+    var shuffle_base : [Int] = [ ]                                           //shuffle後のbase配列格納
+    var yamahuda : [Int] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,122,11,52,51] //山札
+    var bahuda : [Int] = [73,14,12,13,43,34,0,0]                             //場札
+    var bahuda_max = 8                                                       //場札最大枚数
+    var yamahuda_nokori = 24                                                 //山札残数
     var huda_check = 0                                                       //任意の札枚数カウント
     var huda_flag : [Int] = [0,0,0,0,0,0,0,0]                                //任意の札の位置を記憶
-    var tmp1 = 0                                                             //一時格納変数
-    var tmp2 = 0                                                             //一時格納変数
-    var base_num : UInt32 = 48
-    var tmp3 = 47
-    var tmp4 = 0
+    var pair_cnt = 0                                                         //ペア数カウント用変数
+    var bahuda_tmp = 0                                                       //場札の一時格納変数
+    var draw_ytmp = 0                                                        //Draw関数の山札格納変数
+    var draw_btmp = 0                                                        //Draw関数の場札格納変数
     
     
     
-    func Bahuda_check(huda_num: Int) -> Int {                               //選択した手札と合う場札が無いか検索
+    func Bahuda_check(huda_num: Int) -> Int {                               //選択した手札と合う場札が無いか検索する関数
         
         
         let huda_num = huda_num / 10
@@ -221,7 +310,7 @@ class Table {
         switch huda_num{
             
         case 1:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 10 < bahuda[i] && bahuda[i] < 15{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
@@ -229,77 +318,77 @@ class Table {
             }
 
         case 2:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 20 < bahuda[i] && bahuda[i] < 25{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
                 }
             }
         case 3:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 30 < bahuda[i] && bahuda[i] < 35{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
                 }
             }
         case 4:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 40 < bahuda[i] && bahuda[i] < 45{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
                 }
             }
         case 5:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 50 < bahuda[i] && bahuda[i] < 55{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
                 }
             }
         case 6:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 60 < bahuda[i] && bahuda[i] < 65{
                     huda_check += 1
                     huda_flag[i] = bahuda[i]
                 }
             }
         case 7:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 70 < bahuda[i] && bahuda[i] < 75{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
                 }
             }
         case 8:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 80 < bahuda[i] && bahuda[i] < 85{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
                 }
             }
         case 9:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 90 < bahuda[i] && bahuda[i] < 95{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
                 }
             }
         case 10:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 100 < bahuda[i] && bahuda[i] < 105{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
                 }
             }
         case 11:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 110 < bahuda[i] && bahuda[i] < 115{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
                 }
             }
         case 12:
-            for i in 0..<bahuda_nokori{
+            for i in 0..<bahuda_max{
                 if 120 < bahuda[i] && bahuda[i] < 125{
                     huda_flag[i] = bahuda[i]
                     huda_check += 1
@@ -312,52 +401,68 @@ class Table {
         }
         
         if huda_check != 0{
-            for i in 0..<bahuda_nokori {
+            for i in 0..<bahuda_max {
                 if huda_flag[i] != 0 {
                     print(huda_flag[i])
-                    tmp1 = i
+                    pair_cnt = i
                 }
             }
         }else{
             print("ペアとなる札が存在しません")
         }
         
-            return tmp1
+            return pair_cnt
         
     }
     
-    func Bahuda_select(select_bnum: Int) -> Int {
+    func Bahuda_select(select_bnum: Int) -> Int {           //選択した場札を場札配列から削除しその場札を返り値にする関数
         
-        tmp1 = bahuda[select_bnum]
+        bahuda_tmp = bahuda[select_bnum]
         bahuda[select_bnum] = 0
     
         
-        return tmp1
+        return bahuda_tmp
+    }
+    
+    
+    func Add_bahuda_y(y: Int) -> Int {                      //空いている場札配列に選択した山札を挿入
+        
+        for i in 0 ..< 8 {
+            
+            if bahuda[i] == 0 {
+                bahuda[i] = y
+                return 0
+            }
+        }
+        
+        return 0
+        
     }
     
     func Draw(player_num: Int) -> Int {            //山札からDrawを行う関数
-        tmp3 = 0
+        print("true")
+        draw_btmp = 0
         
-        tmp2 = yamahuda[yamahuda_nokori - 1]
+        draw_ytmp = yamahuda[yamahuda_nokori - 1]
         yamahuda[yamahuda_nokori - 1] = 0
         yamahuda_nokori -= 1
         
-        for i in 0 ..< bahuda_nokori {
-            if tmp2/10 == bahuda[i]/10 {
-                tmp3 = Bahuda_select(i)
+        for i in 0 ..< bahuda_max {
+            if draw_ytmp/10 == bahuda[i]/10 {
+                print("true2")
+                draw_btmp = Bahuda_select(i)
                 
                 if player_num == 1 {
-                    player1.Add_syutoku_yb(tmp2, select_b: tmp3)
+                    player1.Add_syutoku_yb(draw_ytmp, select_b: draw_btmp)
                 }else if player_num == 2 {
-                    player2.Add_syutoku_yb(tmp2, select_b: tmp3)
+                    player2.Add_syutoku_yb(draw_ytmp, select_b: draw_btmp)
                 }
                 
                 return 0
             }
         }
-        
-        bahuda[bahuda_nokori] = tmp2
-        bahuda_nokori += 1
+        print(draw_ytmp)
+        Add_bahuda_y(draw_ytmp)
         
         return 1
     }
@@ -442,16 +547,12 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.whiteColor()
         
-        player1.player_name = "player1"
-        player2.player_name = "player2"
-        player1.tehuda_nokori = 7
+        player1.player_num = 1
+        player2.player_num = 2
         print("null")
       //  table.Shuffle()
-
         
-        for test2 in player1.syutoku {
-            print(test2)
-        }
+
         
         for test2 in table.bahuda {
             print(test2)
@@ -460,16 +561,24 @@ class GameViewController: UIViewController {
         for test2 in table.yamahuda {
             print(test2)
         }
-        
-
-        
-        table.Draw(1)
-        for test2 in table.shuffle_base {
+  
+                print("")
+        for test2 in player1.syutoku {
             print(test2)
         }
+
+        
+        player1.CP()
+                print("")
+                print(player1.syutoku_maisu)
+    
+
 
         print("")
         
+        print("")
+        
+        
 
         
         for test2 in table.bahuda {
@@ -479,25 +588,13 @@ class GameViewController: UIViewController {
         for test2 in table.yamahuda {
             print(test2)
         }
-        
+        print("")
         for test2 in player1.syutoku {
             print(test2)
         }
         
         print("")
-        table.Draw(1)
-        
-        for test2 in table.bahuda {
-            print(test2)
-        }
-        
-        for test2 in table.yamahuda {
-            print(test2)
-        }
-        
-        for test2 in player1.syutoku {
-            print(test2)
-        }
+
         
 //        for test3 in player1.tehuda {
 //            print(test3)
